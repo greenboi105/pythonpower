a62ad7fa103995012835bef02d6a2e91f076ed27
greenboi105 <67177162+greenboi105@users.noreply.github.com>
2022-05-31 15:11:26 -0400

    Update power-in-python.c

--- power-in-python.c
+++ power-in-python.c
@@ -3,14 +3,14 @@ We want to read in pairs of integers n and e from stdin and compute n^e.
 We use fork and exec to create a child process then upgrade the child process to execute
 an interactive python interpreter that does the actual computation.
 
-Power-in-python must create two pipes to communicate with the child process, one for sending expressions to python and one for reading 
-back the results. 
+Power-in-python must create two pipes to communicate with the child process, one for sending expressions to python and one for reading back the results. 
 
 The stdin and stdout of the child process should be redirected to the proper pipe ends.
 The stderr of the child python process should be redirected to the special file /dev/null.
 
-The communications between the power-in-python process and the python process are done by text lines,
-each ending with a newline. 
+The communications between the power-in-python process and the python process are done by text lines, each ending with a newline. 
+
+Header files used include files for I/O, UNIX system calls, error checking, and string functions.
 */
 
 #define _POSIX_C_SOURCE 200809L
@@ -27,14 +27,20 @@ each ending with a newline.
 
 int main(int argc, char ** argv)
 {
+   // the command we want to use with exec to run the python interpreter in the child process 
    char *cmd = "python3";
-   int parenttochild[2], childtoparent[2]; // two element arrays for pipes
+   
+   // two element arrays for pipes
+   int parenttochild[2], childtoparent[2]; 
+   
+   // the process ID to determine if we are in the child or parent 
    pid_t pid;
 
    // two pipes, one for sending from parent to child and one for sending from child to parent
    pipe(parenttochild);
    pipe(childtoparent);
    
+   // fork a child process 
    pid = fork();
 
    // entering the child process and preparing it for the exec call
@@ -52,12 +58,12 @@ int main(int argc, char ** argv)
       dup2(childtoparent[1], 1);
       dup2(devnull, 2);
       
-      // close the other pipe ends
+      // close the other pipe ends once the redirection is complete 
       close(parenttochild[0]);
       close(childtoparent[1]);
       close(devnull);
 
-      // call the python3 command
+      // call the python3 command with execlp 
       execlp(cmd, cmd, "-i", NULL);
 
    } else {
@@ -68,22 +74,31 @@ int main(int argc, char ** argv)
 
       while (1) {
 
-         // get input so long as the user is entering it 
+         // If the user does not enter input in the form of two numbers, break out of the loop due to invalid input formatting 
          if (scanf("%d %d", &number1, &number2) != 2)
             break; // break out
-
-         char bufferout[MAXLINE]; // declare a buffer for reading out
+         
+         // declare a buffer for sending information out 
+         char bufferout[MAXLINE]; 
+         
+         // declare a buffer for reading in information; buffer is initialized to all zeroes 
          char bufferin[MAXLINE] = {0};
-
+         
+         // deposit formatted string into the buffer for writing information to the child 
          sprintf(bufferout, "(%d) ** (%d)\n", number1, number2);
-
+      
+         // send the formatted string through the pipe to the child 
+         // parameters of the write system call are the file descriptor (write end of the pipe), the buffer whose contents are being sent and the size of the contents
          write(parenttochild[1], bufferout, strlen(bufferout));
-
+         
+         // read what was sent back from the child using the read end of the pipe, the buffer for storing what was read and the max potential size 
          read(childtoparent[0], bufferin, 1024);
-
+         
+         // print what was received back 
          printf("(%d) ** (%d) = %s", number1, number2, bufferin);
       }
-
+      
+      // close unneeded file descriptors 
       close(parenttochild[1]);
       close(childtoparent[0]);
 
